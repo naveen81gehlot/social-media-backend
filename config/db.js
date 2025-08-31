9830f50192bc56c6043910352efd18fb0b132d5a
Devanshu <sonbhurra@gmail.com>
2025-08-31 23:58:12 +0530

    Update db connection for Atlas

--- config/db.js
+++ config/db.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const connectDb=async ()=>{
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/socialmedia")
+        await mongoose.connect(process.env.MONGO_URI,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
         console.log("database is connectd");
         
     } catch (error) {
